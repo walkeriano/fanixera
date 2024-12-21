912d0e78ef802aa79ba33c78823de77d6525541f
walkeriano <awalkerbarreda@gmail.com>
2024-12-22 00:58:53 +0100

    integration viewall styles

--- src/components/viewall/viewall.jsx
+++ src/components/viewall/viewall.jsx
@@ -3,7 +3,22 @@ import styles from "./viewall.module.css"
 export default function viewall(){
     return(
         <section className={styles.viewall}>
-            viewall
+            <section className={styles.rieles}>
+                <div className={styles.boxad}></div>
+                <div className={styles.boxad}></div>
+                <div className={styles.boxad}></div>
+                <div className={styles.boxad}></div>
+                <div className={styles.boxad}></div>
+                <div className={styles.boxad}></div>
+            </section>
+            <section className={styles.rieles}>
+                <div className={styles.boxad}></div>
+                <div className={styles.boxad}></div>
+                <div className={styles.boxad}></div>
+                <div className={styles.boxad}></div>
+                <div className={styles.boxad}></div>
+                <div className={styles.boxad}></div>
+            </section>
         </section>
     )
 }
