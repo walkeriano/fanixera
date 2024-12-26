3c62691985e6dfc8e73e5d6aa5aa01f36d491f88
walkeriano <awalkerbarreda@gmail.com>
2024-12-26 18:41:44 +0100

    calibración de tamaños

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Header() {
   return (
     <section className={styles.header}>
-      <Image src="/next.svg" width={115} height={60} alt="logo-fanixera" />
+      <Image src="/next.svg" width={90} height={45} alt="logo-fanixera" />
       <section className={styles.sectionbtns}>
         <Link href="/" className={styles.btnmenu}>
           <Image src="/globe.svg" alt="icon-menu" width={30} height={20} />
