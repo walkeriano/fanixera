dc75c45d89cd747dfddba1259cfdc12b8aa51a5c
walkeriano <awalkerbarreda@gmail.com>
2025-01-17 00:37:15 +0100

    ready cambios animacion

--- src/components/tools/tools.jsx
+++ src/components/tools/tools.jsx
@@ -1,56 +1,29 @@
 import React, { useState } from "react";
 import styles from "./tools.module.css";
-import Link from "next/link";
 import Image from "next/image";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faXmark, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 export default function Tools({ onRandomize }) {
-  const [searchSpace, setSearchSpace] = useState(true);
+  const [isSpinning, setIsSpinning] = useState(false);
+
+  const handleButtonClick = () => {
+    setIsSpinning(true); // Iniciar la animación
+    onRandomize(); // Ejecutar la función pasada como prop
+
+    // Remover la clase de animación después de que termine
+    setTimeout(() => setIsSpinning(false), 2000); // Duración de la animación: 1s
+  };
 
   return (
     <section className={styles.btnSection}>
-      {searchSpace ? (
-        <section
-          onClick={() => setSearchSpace(false)}
-          className={styles.btnitemSelect}
-        >
-          <FontAwesomeIcon
-            icon={faMagnifyingGlass}
-            size="2x"
-            className={styles.icon}
-          />
-        </section>
-      ) : (
-        <section className={styles.containerSearch}>
-          <button
-            className={styles.btnBack}
-            onClick={() => setSearchSpace(true)}
-          >
-            <FontAwesomeIcon icon={faXmark} size="2x" className={styles.icon} />
-          </button>
-          <section className={styles.boxInput}>
-            <input
-              type="text"
-              placeholder="Buscar..."
-              className={styles.inputSearch}
-            />
-            <button className={styles.btnSearch}>
-              <FontAwesomeIcon
-                icon={faMagnifyingGlass}
-                size="2x"
-                className={styles.icon}
-              />
-            </button>
-          </section>
-        </section>
-      )}
-      <button onClick={onRandomize} className={styles.btnAleatorio}>
-        <Image src="/window.svg" alt="icon-random" width={53} height={53} />
+      <button onClick={handleButtonClick} className={styles.btnAleatorio}>
+        <Image
+          className={isSpinning ? styles.spin : ""}
+          src="/window.svg"
+          alt="icon-random"
+          width={53}
+          height={53}
+        />
       </button>
-      <Link href="/" className={styles.btnitemSelect}>
-        <Image src="/user.svg" alt="icon-random" width={25} height={25} />
-      </Link>
     </section>
   );
 }
