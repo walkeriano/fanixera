655ad372dfa53bd04c5f54b63712802cd8f9b0c9
walkeriano <awalkerbarreda@gmail.com>
2025-01-17 19:59:08 +0100

    loader new

--- src/components/loaderIntro/loaderIntro.jsx
+++ src/components/loaderIntro/loaderIntro.jsx
@@ -4,7 +4,7 @@ export default function IntroLoader() {
   return (
     <section className={styles.containerLoader}>
       <Image src="/next.png" width={200} height={80} alt="logo-fanixera" />
-      <h1>TOMI CIBERMARKETING</h1>
+      <h1>TOMI CIBERMARKETING - 2025</h1>
       <div className={styles.loader}></div>
     </section>
   );
